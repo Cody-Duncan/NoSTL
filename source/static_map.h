ffa87d5fd37edcbd90ccb710d5c4759874fe7754
Cody Duncan <CodyDuncan1260@live.com>
2015-11-29 19:28:26 -0800

    beginning of static_map

--- source/static_map.h
+++ source/static_map.h
@@ -13,11 +13,20 @@ Description:
 #define STATIC_MAP_H
 
 // ---------- includes ---------
-
+#include "primitives.h"
+#include "base_array.h"
+#include <functional> // std::Hash, std::equal_to
 
 namespace nostl
 {
+	template <class Key, class Value, uint max_length, typename Hash = std::hash<Key>, typename Equality = std::equal_to<Key>>
+	class static_map : protected base_array<T, static_map<Key, Value, max_length, Hash, Equality>>
+	{
+	public:
+		static_map() = default;
 
+		
+	};
 }
 
 #endif
